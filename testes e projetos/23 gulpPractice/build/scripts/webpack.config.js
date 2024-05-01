e021006c8f98e16cf17dc8310b204b228e635e8d
Gabriela Zimmermann <zmngabriela@gmail.com>
2024-04-30 22:10:18 -0300

    fix: minify images

--- testes e projetos/23 gulpPractice/build/scripts/webpack.config.js
+++
@@ -1,2 +0,0 @@
-const path=require("path");module.exports={mode:"development",entry:"./source/scripts/main.js",output:{path:path.resolve(__dirname,"build/scripts"),filename:"main.js"},module:{rules:[{test:/\.scss$/,use:["style-loader","css-loader","sass-loader"]}]}};
-//# sourceMappingURL=maps/webpack.config.js.map
--- testes e projetos/23 gulpPractice/source/scripts/main.js
+++ testes e projetos/23 gulpPractice/source/scripts/main.js
@@ -1,16 +1,9 @@
-// import styles from './source/script/variantes.scss';
+/*import './source/script/variantes.scss';*/
 
 document.addEventListener('DOMContentLoaded', function() {
     const buttonClick = document.querySelector('button');
 
     buttonClick.addEventListener('click', function() {
-        buttonClick.style.backgroundColor = '#ff009d'/*styles.corTerciaria*/;
+        buttonClick.style.backgroundColor = "#ff009d"/*corTerciaria*/;
     });
 });
-
-// eu tentei usar o import './source/scripts/variantes.scss';
-// pra usar as cores do variantes.scss (la eu usei o export)
-// isso seria usando tambem os pacotes "style-loader","css-loader","sass-loader"
-// as cores funcionam normal no main.css, mas no js nao consigo importar
-// no dev tools do chrome diz Uncaught SyntaxError: Cannot use import statement outside a module (at main.js:1:1) relativo ao import styles from "./source/script/variantes.scss";
-// depois de perder meio dia so vendo foruns online e nao conseguir resolver,  to mandando assim pois acho que ja resolve a pratica :)
